e2ea28b40259bb210c310503284245e3b8b37eef
Bruno DaSilva <bananaatomica@Brunos-MacBook-Air.local>
2020-01-26 21:25:31 -0600

    Added style to vp, STARTED js logic

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 console.log("Ciao Mundo");
+
+/* Get Our Elements */
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+
+/* Build out functions */
+function togglePlay() {
+  const method = video.paused ? "play" : "pause";
+  video[method]();
+}
+
+function updateButton() {
+  const icon = this.paused ? "►" : "❚ ❚";
+  console.log(icon);
+  toggle.textContent = icon;
+}
